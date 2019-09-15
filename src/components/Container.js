0427937f2ec1314f9c0206c8851e256f05e9ad8c
Thiago kauai <html5ephp@gmail.com>
2019-09-14 23:05:05 -0300

    commit saturday

---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './Counter'
+
+const container = () => {
+    return <> 
+            <div>
+                <Counter/>
+            </div>
+          </>
+}
+
+export default container
---
+++ src/components/Counter.js
@@ -0,0 +1,8 @@
+import React,{ } from 'react';
+const counter = () => {
+    return <>
+      <h1>App</h1>
+    </>
+}
+
+export default counter
